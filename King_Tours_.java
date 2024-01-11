7b7c5553c383c1aa39bdb29716857824c82cbbf2
SSiidduu-2005 <132639773+SSiidduu-2005@users.noreply.github.com>
2024-01-11 12:06:53 +0530

    import java.util.*; public class Hi{     public static void main(String[] args){         Scanner sc=new Scanner(System.in);         int x=sc.nextInt();         int y=sc.nextInt();         int z=(x*5)+(y*7);         System.out.println(z);     } }

---
+++ King_Tours_.java
@@ -0,0 +1,10 @@
+import java.util.*;
+public class Hi{
+    public static void main(String[] args){
+        Scanner sc=new Scanner(System.in);
+        int x=sc.nextInt();
+        int y=sc.nextInt();
+        int z=(x*5)+(y*7);
+        System.out.println(z);
+    }
+}
