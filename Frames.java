e5c45bb9c455b47d2c63a255e930507ca7c56f72
SSiidduu-2005 <132639773+SSiidduu-2005@users.noreply.github.com>
2024-01-11 12:02:04 +0530

    import java.util.*; public class Hi{     public static void main(String[] args){         Scanner sc=new Scanner(System.in);         int x=sc.nextInt();         int y=sc.nextInt();         int z=sc.nextInt();         int a=2*(x+y)*z;         System.out.print(a);     } }

---
+++ Frames.java
@@ -0,0 +1,11 @@
+import java.util.*;
+public class Hi{
+    public static void main(String[] args){
+        Scanner sc=new Scanner(System.in);
+        int x=sc.nextInt();
+        int y=sc.nextInt();
+        int z=sc.nextInt();
+        int a=2*(x+y)*z;
+        System.out.print(a);
+    }
+}
