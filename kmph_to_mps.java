a36d667f65728f09c34354ef6264751775307764
SSiidduu-2005 <132639773+SSiidduu-2005@users.noreply.github.com>
2024-01-12 11:41:51 +0530

    import java.util.*; public class ex5 {     public static void main(String[] args){         Scanner sc=new Scanner(System.in);         double r=sc.nextDouble();         double k=(r*5)/18;         System.out.printf("%.2f",k);     } }

---
+++ kmph_to_mps.java
@@ -0,0 +1,10 @@
+import java.util.*;
+public class ex5 {
+    public static void main(String[] args){
+        Scanner sc=new Scanner(System.in);
+        double r=sc.nextDouble();
+        double k=(r*5)/18;
+        System.out.printf("%.2f",k);
+    }
+}
+   
