46e6fdf63ec5bbbd81460e0cf5e848901c75de28
SSiidduu-2005 <132639773+SSiidduu-2005@users.noreply.github.com>
2024-01-12 11:37:39 +0530

    import java.util.*; public class ex5 {     public static void main(String[] args){         Scanner sc=new Scanner(System.in);         int r=sc.nextInt();         int s=sc.nextInt();         int k=2*r-s;         System.out.printf("%d",k);     } }

---
+++ Find_Second_Number.java
@@ -0,0 +1,11 @@
+import java.util.*;
+public class ex5 {
+    public static void main(String[] args){
+        Scanner sc=new Scanner(System.in);
+        int r=sc.nextInt();
+        int s=sc.nextInt();
+        int k=2*r-s;
+        System.out.printf("%d",k);
+    }
+}
+   
