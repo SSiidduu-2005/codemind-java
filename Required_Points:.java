dd52c9a6b37f0d299c1769e69e3a074d0e365365
SSiidduu-2005 <132639773+SSiidduu-2005@users.noreply.github.com>
2024-01-12 10:52:05 +0530

    import java.util.*; public class ex5 {     public static void main(String[] args){         Scanner sc=new Scanner(System.in);         int r=sc.nextInt();         int s=sc.nextInt();         int j=s/r;         System.out.printf("%d",j);     } }

---
+++ Required_Points:.java
@@ -0,0 +1,11 @@
+import java.util.*;
+public class ex5 {
+    public static void main(String[] args){
+        Scanner sc=new Scanner(System.in);
+        int r=sc.nextInt();
+        int s=sc.nextInt();
+        int j=s/r;
+        System.out.printf("%d",j);
+    }
+}
+   
