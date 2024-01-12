8a762b4f88ea6885db3ee5da05a761d43f09b35f
SSiidduu-2005 <132639773+SSiidduu-2005@users.noreply.github.com>
2024-01-12 10:37:42 +0530

    import java.util.*; public class ex5 {     public static void main(String[] args){         Scanner sc=new Scanner(System.in);         double r=sc.nextDouble();         double b=(4*3.14*r*r*r)/3;         System.out.printf("%.2f",b);     } }

---
+++ Volume_of_Sphere.java
@@ -0,0 +1,10 @@
+import java.util.*;
+public class ex5 {
+    public static void main(String[] args){
+        Scanner sc=new Scanner(System.in);
+        double r=sc.nextDouble();
+        double b=(4*3.14*r*r*r)/3;
+        System.out.printf("%.2f",b);
+    }
+}
+   
