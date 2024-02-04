2907fda1942290470a4718d4e291f6c4ecdee7cb
SSiidduu-2005 <132639773+SSiidduu-2005@users.noreply.github.com>
2024-02-04 09:30:44 +0530

    import java.util.*; public class hi{     public static void main(String[] args){         Scanner sc=new Scanner(System.in);         int n=sc.nextInt();         int t=(n*(n-1))/2;         System.out.println(t);     } }

---
+++ Program_to_find_the_maximum_number_of_handshakes_.java
@@ -0,0 +1,9 @@
+import java.util.*;
+public class hi{
+    public static void main(String[] args){
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        int t=(n*(n-1))/2;
+        System.out.println(t);
+    }
+}
