86f4801a54c977d132bd99a96bf0b8a784605d6d
SSiidduu-2005 <132639773+SSiidduu-2005@users.noreply.github.com>
2024-02-04 09:59:16 +0530

    import java.util.*; public class hi{     public static void main(String[] args){         Scanner sc=new Scanner(System.in);         double pi=3.14;         float r=sc.nextFloat();         double c=pi*r*r;         System.out.printf("%.2f",c);     } }

---
+++ Area_of_a_circle.java
@@ -0,0 +1,10 @@
+import java.util.*;
+public class hi{
+    public static void main(String[] args){
+        Scanner sc=new Scanner(System.in);
+        double pi=3.14;
+        float r=sc.nextFloat();
+        double c=pi*r*r;
+        System.out.printf("%.2f",c);
+    }
+}
